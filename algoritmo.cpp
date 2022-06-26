c2a688b30f749883aab836cb76b4ddd7895c1233
Joxxx69 <santiago.gualotuna@hotmail.com>
2022-06-25 20:10:54 -0500

    cambios

--- algoritmo.cpp
+++ algoritmo.cpp
@@ -4,97 +4,144 @@
 #include<cmath>
 
 using namespace std;
-
-bool revisionDominios();
+int combinaciones(int, int);
 int factorial(int);
 void estadoInicial();
-int combinaciones(int, int);
-bool unicoIndice(int,int[]);
+bool RestriccionBinaria(int []);
+void restriccionGlobal();
 
-string X[]={"F", "T", "U", "W", "R", "O"};
-string Y[] = {"C1", "C2", "C3"};
 struct VariableAux{
-    string nombre;
-    int valor;
+    string nombre;  //{"C1", "C2", "C3"};
+    int valor;      //C1=?  C2 =?  C3=F
 }variableAux[3];
 
-//{"F", "T", "U", "W", "R", "O"};
-//{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-struct Variable{
-    string nombre;
-    int valor;
-}variable[6];
 
-struct DominiosEstados{
-    int valor;
-    bool estado;
-}dominio[10];
+struct Variables{
+    string nombre;  //{"F", "T", "U", "W", "R", "O"}
+    int valor;     // valores del dominio
+} variable[6];
+
 
 int main(){
     estadoInicial();
-    //int random = rand() % 9;
-    //variable[0].valor = dominio[random].valor;
-    //dominio[random].estado = false;
-    for (int i = 0; i < 6;i++){
-        variable[i].valor = dominio[i].valor;
-        dominio[i].estado = false;
+    restriccionGlobal();
+    cout << "!Solucion encontrada!" << endl;
+    cout << "   T W O "<<endl;      
+    cout << "+  T W O "<<endl;    
+    cout << " _______ "<<endl;     
+    cout << " F O U R "<<endl;     
+    cout << endl;
+    
+    for(int i = 0; i < 6; i++){
+        cout <<variable[i].nombre<<": "<<variable[i].valor << endl;
     }
-    if(variable[0].valor == 0){
-        for (int i = 0; i < 10;i++){
-            if(dominio[i].estado){
-                variable[0].valor = dominio[i].valor;
+    return 0;
+}
+
+
+
+//-------------------------------------Inicializacion de Estados---------------------------------------------
+void estadoInicial(){
+    string X[]={"F", "T", "U", "W", "R", "O"};
+    string Y[] = {"C1", "C2", "C3"};
+    for (int i = 0; i < 3;i++){ // ingreso de nombres y valores a las variables auxiliares
+        variableAux[i].nombre = Y[i];
+        variableAux[i].valor = (i == 2) ? 1 : 0;
+    }
+    for (int i = 0; i < 6;i++){  // ingreso de nombres y valores iniciales a las variables
+        variable[i].nombre = X[i];
+        variable[i].valor = (i == 0) ? 1 : 0;
+    }
+
+}
+
+//-----------------------------Restricciones Unarias, Binarias, Globles-----------------------------------------
+bool RestriccionBinaria(int estados[6]){
+    for(int i = 0; i < 5; i++){
+        for(int j = i + 1; j < 6; j++){
+            if(estados[i] == estados[j]){
+                return false;
             }
         }
     }
-    cout << endl;
-    for (int i = 0; i < 6;i++){
-        cout << variable[i].nombre<<": ";
-        cout << variable[i].valor << endl;
-    }
-
-        return 0;
+    return true;
 }
+void restriccionGlobal(){
+    int estados[6];
+     // las Cs tienen valores de 0 - 1 excepto C3=1 --> por la restriccion F = C3 --> F!=0
+    // por la restriccion F = C3 = 1;
+    for(int i =0;i<10;i++){
+        variable[0].valor = variableAux[2].valor;  // valor de F y C3 --> F=C3
+        variable[5].valor = i;                        //valor de O --> 0,1,2,3,4,5,6,7,8,9
+        variable[4].valor = (2 * variable[5].valor > 9)?2*variable[5].valor-10: 2*variable[5].valor;  //valor de  R --> 0,2,4,6,8,0,2,4,6,8
+        variableAux[0].valor = (2 * variable[5].valor > 9) ? 1 : 0;  // valor de la C1
+        variable[1].valor = 10 + variable[5].valor;
+        variableAux[1].valor = ((variable[1].valor)%2==0) ? 0: 1; //valor de C2
+        variable[1].valor = (variable[5].valor +10  - variableAux[1].valor) / 2; // valor de T -> T= (O+10-C2)/2
 
-//-------------------------------------Inicializacion de Estados-----------------------------------------------------------------
-void estadoInicial(){
-    srand(time(0));
-    for (int i = 0; i < 6;i++){ // ingreso de nombres de variables
-        variable[i].nombre = X[i];
-    }
-    for (int i = 0; i < 3;i++){ // ingreso de nombres de variables auxiliares
-        variableAux[i].nombre = Y[i];
-    }
-    int n, num[10];
-    for(int i=0; i<10; i++){  // ingresa datos del 0 - 9 desordenadamente en el arreglo
-        do{
-            n = rand() % 10;
-        }while(unicoIndice(n, num));
-        num[i] = n;
-        dominio[i].valor = num[i];
-        dominio[i].estado = true;
-    }
-    for (int i = 0; i < 10;i++){
-        cout << dominio[i].valor << "  ";
-    }
-}
-bool unicoIndice(int n, int num[]){
-    for (int i = 0; i < 10;i++){
-        if(n== num[i]){
-            return true;
+        for (int j = 0; j < 10;j++){
+            variable[3].valor = j;  //valor de w                                    
+            variable[2].valor = ((((2 * variable[3].valor)+variableAux[0].valor))%10); // valor de U=((2*W)+C1)- 10*C2
         }
+        for (int idx = 0; idx < 6;idx++){
+            estados[idx] = variable[idx].valor;
+        }
+        int valor1 = (variable[1].valor);
+        int valor2 =((variable[5].valor +10  - variableAux[1].valor)/2);
+        if(RestriccionBinaria(estados)){
+            if(valor1 == valor2){
+                break;
+            }
+        }       
     }
-    return false;
+
 }
-//---------------------------------------------Asignacion de valores---------------------------------------------------------
+//struct Variables{
+//    string nombre;
+//    int valor;
+//    bool estado;
+//} variable[210][6];
+//int contador=0;
+//void combinacionSR(int*datos,int n,int r,int y=0,int i=0,int *indexs=NULL){ // esta funcion me da convinaciones de dominios sin repeticion
+//    if(indexs == NULL){
+//        indexs = new int[r];
+//    }
+//    if(i<r){
+//        for(int x=y;x<n;x++){
+//            indexs[i]=x;
+//            combinacionSR(datos,n,r,x+1,i+1,indexs);
+//        }
+//    }else{
+//        for(int w=0;w<r;w++){
+//            variable[contador][w].valor = datos[indexs[w]];
+//            variable[contador][w].estado = true;
+//        }
+//        contador++;
+//    }
+//}
 
+//void estadoInicial(){
+//    int dominio[] = {0,1,2,3,4,5,6,7,8,9}; 
+//    combinacionSR(dominio, 10, 6);  // se obtienen 210 combinaciones sin repeticion de dominios 
 
+//    for (int i = 0; i < 210;i++){ // se ingresan nombres a las 210 variables 
+//        for (int j = 0; j < 6;j++){
+//            variables[i][j].nombre = X[j];
+//            variable[i][j].estado = true;
+//        }
+//    }
+//    for (int i = 0; i < 3;i++){ // ingreso de nombres de variables auxiliares
+//        variableAux[i].nombre = Y[i];
+//    }
 
-
-
-
-
-
-
+//    //cout <<endl<<"el contador es: "<<contador<<endl;
+//    //for (int i = 0; i < 210;i++){
+//    //    for (int j = 0; j < 6;j++){
+//    //        cout << variable[i][j].valor << "  ";
+//    //    }
+//    //    cout << endl;
+//    //}
+//}
 
 
 
