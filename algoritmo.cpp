645495e327ff6b5e31daee8639fdc90b84b7955a
Joxxx69 <santiago.gualotuna@hotmail.com>
2022-06-29 11:30:39 -0500

    listo

--- algoritmo.cpp
+++ algoritmo.cpp
@@ -76,7 +76,11 @@ void restriccionGlobal(){
     for(int i =0;i<10;i++){
         variable[0].valor = variableAux[2].valor;  // valor de F y C3 --> F=C3
         dominio[1].estado = false;
-        variable[5].valor = dominio[i].valor;                        //valor de O --> 0,1,2,3,4,5,6,7,8,9
+        if(dominio[i].estado){
+            variable[5].valor = dominio[i].valor;                        //valor de O --> 0,1,2,3,4,5,6,7,8,9
+        }else{
+            continue;
+        }
         dominio[i].estado = false;
         variable[4].valor = (2 * variable[5].valor > 9)?2*variable[5].valor-10: 2*variable[5].valor;  //valor de  R --> 0,2,4,6,8,0,2,4,6,8
         variableAux[0].valor = (2 * variable[5].valor > 9) ? 1 : 0;  // valor de la C1
